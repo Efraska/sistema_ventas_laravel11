5f46721d99bd0fecbbd5bdcc8a52ca1dc12824e9
Efraska <aux_sistemas@medicainsurgentes.com.mx>
2025-02-24 22:34:41 -0600

    Se implemento la funcionalidad para eliminar categorías, metodo destroy y configuramos la ruta de eliminación

--- app/Http/Controllers/Categorias.php
+++ app/Http/Controllers/Categorias.php
@@ -44,7 +44,9 @@ class Categorias extends Controller
      */
     public function show(string $id)
     {
-        //
+        $titulo = 'Eliminar categoría';
+        $item = Categoria::find($id);
+        return view('modules.categorias.show', compact('item', 'titulo'));
     }
 
     /**
@@ -68,6 +70,8 @@ class Categorias extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $item = Categoria::find($id);
+        $item->delete();
+        return to_route('categorias');
     }
 }
